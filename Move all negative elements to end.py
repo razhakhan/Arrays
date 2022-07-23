15f792ba6352b0b9e303cc6ec9ef1bdb56b94913
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-07-23 11:36:26 +0530

    Create Move all negative elements to end.py

---
+++ Move all negative elements to end.py
@@ -0,0 +1,63 @@
+#User function Template for python3
+
+class Solution:
+    def segregateElements(self, arr, n):
+        arr1=[]
+        arr2=[]
+        for i in arr:
+            if i>=0:
+                arr1.append(i)
+            else:
+                arr2.append(i)
+        arr.clear()
+        arr1.extend(arr2)
+        arr.extend(arr1)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.segregateElements(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
+
+'''
+
+Example 1:
+
+Input : 
+N = 8
+arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+Output : 
+1  3  2  11  6  -1  -7  -5
+
+Example 2:
+
+Input : 
+N=8
+arr[] = {-5, 7, -3, -4, 9, 10, -1, 11}
+Output :
+7  9  10  11  -5  -3  -4  -1
+
+'''

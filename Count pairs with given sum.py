4c068c3cbf06557ef14b987e92a687acb0ff1390
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-08-06 11:21:31 +0530

    Create Count pairs with given sum.py

---
+++ Count pairs with given sum.py
@@ -0,0 +1,55 @@
+'''
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+
+Example 2:
+
+Input:
+N = 4, K = 2
+arr[] = {1, 1, 1, 1}
+Output: 6
+Explanation: 
+Each 1 will produce sum 2 with any 1.
+
+'''
+
+
+
+#User function Template for python3
+
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        from collections import defaultdict
+        dic={}
+        dic=defaultdict(lambda:0,dic)
+        c=0
+        for a in arr:
+            b=k-a
+            if b in dic:
+                c+=dic[b]
+            dic[a]+=1
+        return c
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getPairsCount(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends

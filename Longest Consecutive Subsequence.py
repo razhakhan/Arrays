a81460f670a0a25433d9fbe1f562ec276b8bc62f
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-08-27 16:11:32 +0530

    Create Longest Consecutive Subsequence.py

---
+++ Longest Consecutive Subsequence.py
@@ -0,0 +1,77 @@
+ #User function Template for python3
+ 
+class Solution:
+    
+    # arr[] : the input array
+    # N : size of the array arr[]
+    
+    #Function to return length of longest subsequence of consecutive integers.
+    def findLongestConseqSubseq(self,arr, N):
+        maxind=max(arr)
+        l=[0]*(maxind+1)
+        for i in arr:
+            l[i]=1
+        maxc=0
+        c=0
+        for i in range(maxind+1):
+            if l[i]==1:
+                c+=1
+            else:
+                c=0
+            maxc=max(maxc, c)
+        return maxc
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n=int(input())
+        a = list(map(int, input().strip().split()))
+        print(Solution().findLongestConseqSubseq(a,n))
+# } Driver Code Ends
+
+
+'''
+
+Example 1:
+
+Input:
+N = 7
+a[] = {2,6,1,9,4,5,3}
+Output:
+6
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6 
+numbers form the longest consecutive
+subsquence.
+Example 2:
+
+Input:
+N = 7
+a[] = {1,9,3,10,4,20,2}
+Output:
+4
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.
+
+'''

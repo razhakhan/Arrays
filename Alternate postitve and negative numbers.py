f9795fd49cc26c483986a2911da7d5a8bb257a3a
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-08-27 13:15:03 +0530

    Create Alternate postitve and negative numbers.py

---
+++ Alternate postitve and negative numbers.py
@@ -0,0 +1,77 @@
+#User function Template for python3
+
+class Solution:
+    def rearrange(self,arr, n):
+        arr1=[] # pos elements
+        arr2=[] # neg elements
+        for i in arr:
+            if i<0:
+                arr2.append(i)
+            else:
+                arr1.append(i)
+        p,ne,i=0,0,0
+        while i<n:
+            if p!=len(arr1):
+                arr[i]=arr1[p]
+                i+=1
+                p+=1
+            if ne!=len(arr2):
+                arr[i]=arr2[ne]
+                i+=1
+                ne+=1
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	tc = int(input())
+	while tc > 0:
+		n = int(input())
+		arr = list(map(int, input().strip().split()))
+		ob = Solution()
+		ob.rearrange(arr, n)
+		for x in arr:
+			print(x, end=" ")
+		print()
+		tc -= 1
+
+# } Driver Code Ends
+
+
+'''
+
+Input: 
+N = 9
+Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
+Output:
+9 -2 4 -1 5 -5 0 -3 2
+Explanation : Positive elements : 9,4,5,0,2
+Negative elements : -2,-1,-5,-3
+As we need to maintain the relative order of
+postive elements and negative elements we will pick
+each element from the positive and negative and will
+store them. If any of the positive and negative numbers
+are completed. we will continue with the remaining signed
+elements.The output is 9,-2,4,-1,5,-5,0,-3,2.
+
+
+Example 2:
+
+Input: 
+N = 10
+Arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+Output:
+5 -5 2 -2 4 -8 7 1 8 0 
+Explanation : Positive elements : 5,2,4,7,1,8,0
+Negative elements : -5,-2,-8
+As we need to maintain the relative order of
+postive elements and negative elements we will pick
+each element from the positive and negative and will
+store them. If any of the positive and negative numbers
+are completed. we will continue with the remaining signed
+elements.The output is 5,-5,2,-2,4,-8,7,1,8,0.
+
+
+'''

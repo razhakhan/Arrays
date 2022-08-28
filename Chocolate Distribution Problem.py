ca3cb347a34389160eaccaa2a20b5b5af35c2494
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-08-28 13:23:05 +0530

    Create Chocolate Distribution Problem.py

---
+++ Chocolate Distribution Problem.py
@@ -0,0 +1,63 @@
+#User function Template for python3
+
+class Solution:
+
+    def findMinDiff(self, arr,N,M):
+        arr=sorted(arr)
+        i=0
+        j=M-1
+        diff=999999999
+        while(j<=N-1):
+            diff=min(diff, arr[j]-arr[i])
+            i+=1
+            j+=1
+        return diff
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        N = int(input())
+        A = [int(x) for x in input().split()]
+        M = int(input())
+
+
+        solObj = Solution()
+
+        print(solObj.findMinDiff(A,N,M))
+# } Driver Code Ends
+
+
+'''
+
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between 
+maximum chocolates and minimum chocolates 
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+Example 2:
+
+Input:
+N = 7, M = 3
+A = {7, 3, 2, 4, 9, 12, 56}
+Output: 2
+Explanation: The minimum difference between
+maximum chocolates and minimum chocolates
+is 4 - 2 = 2 by choosing following M packets :
+{3, 2, 4}.
+
+'''

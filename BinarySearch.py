acf8d54206e856f9d1a31d46664a195ccb9296c9
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-09-22 17:23:57 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -13,3 +13,27 @@ class Solution:
             else:
                 l=mid+1
         return -1
+
+    
+'''
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {1 2 3 4 5} 
+K = 4
+Output: 3
+Explanation: 4 appears at index 3.
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {11 22 33 44 55} 
+K = 445
+Output: -1
+Explanation: 445 is not present.
+
+
+'''
